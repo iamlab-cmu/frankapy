1ff007619cea35bf17bbebc3591ba0381f71558a
Kevin Zhang <kevinleezhang@gmail.com>
2020-03-30 21:26:19 -0400

    Fixed file name.

--- bash_scripts/start_franka_interface_on_control_pc.sh
+++ bash_scripts/start_franka_interface_on_control_pc.sh
@@ -24,7 +24,7 @@ if [ "$control_pc_use_password" = "0" ]; then
 ssh -tt $control_pc_uname@$control_pc_ip_address << EOSSH
 cd $control_pc_franka_interface_path
 cd build
-./main_franka_interface --stop_on_error $stop_on_error
+./franka_interface --stop_on_error $stop_on_error
 bash
 EOSSH
 else
@@ -32,7 +32,7 @@ sshpass -p "$control_pc_password" ssh -tt -o StrictHostKeyChecking=no $control_p
 cd $control_pc_franka_interface_path
 cd build
 echo $stop_on_error
-./main_franka_interface --stop_on_error $stop_on_error
+./franka_interface --stop_on_error $stop_on_error
 bash
 EOSSH
 fi
