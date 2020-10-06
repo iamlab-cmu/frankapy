8751f244dfee00431f73c267502b4cf01a561bec
Kevin Zhang <kevinleezhang@gmail.com>
2020-10-06 14:44:48 -0400

    Forgot to pass over the flag.

--- bash_scripts/start_franka_interface_on_control_pc.sh
+++ bash_scripts/start_franka_interface_on_control_pc.sh
@@ -25,7 +25,7 @@ if [ "$control_pc_use_password" = "0" ]; then
 ssh -tt $control_pc_uname@$control_pc_ip_address << EOSSH
 cd $control_pc_franka_interface_path
 cd build
-./franka_interface --stop_on_error $stop_on_error
+./franka_interface --with_gripper $with_gripper --stop_on_error $stop_on_error
 bash
 EOSSH
 else
@@ -33,7 +33,7 @@ sshpass -p "$control_pc_password" ssh -tt -o StrictHostKeyChecking=no $control_p
 cd $control_pc_franka_interface_path
 cd build
 echo $stop_on_error
-./franka_interface --stop_on_error $stop_on_error
+./franka_interface --with_gripper $with_gripper --stop_on_error $stop_on_error
 bash
 EOSSH
 fi
