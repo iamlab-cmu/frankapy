d66560ef02f35f430772fdc05ffcbf6369b49ebb
Kevin Zhang <kevinleezhang@gmail.com>
2021-08-24 12:03:47 -0400

    Fixed minor syntax error.

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,7 @@ import mock
  
 MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot', 'scipy.interpolate']
 for mod_name in MOCK_MODULES:
-sys.modules[mod_name] = mock.Mock()
+    sys.modules[mod_name] = mock.Mock()
 
 # -- Project information -----------------------------------------------------
 
